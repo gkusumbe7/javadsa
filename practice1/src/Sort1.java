0692ce8c27a3c4cfd5c9f8dd91858e2a08eb3319
gkusumbe7 <gkusumbe7@gmail.com>
2023-12-01 23:06:18 +0530

    add

---
+++ practice1/src/Sort1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Sort1 {
+    public static void main(String[] args) throws Exception {
+         Scanner scn = new  Scanner(System.in);
+          int n = scn.nextInt();
+          for(int i =  1 ; i < n ; i++){
+              System.out.println(i);
+          }
+    }
+}
